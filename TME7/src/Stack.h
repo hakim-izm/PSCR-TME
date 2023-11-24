006812ba0fb9a7edf0a61913d4d5bd46d0444bd0
hakim-izm <hakim.izm@etu.sorbonne-universite.fr>
2023-11-24 15:48:03 +0100

    Avancement TME7 - SIGINT Stack

--- TME7/src/Stack.h
+++ TME7/src/Stack.h
@@ -3,49 +3,67 @@
 #include <cstring> // size_t,memset
 #include <semaphore.h>
 
+#include <iostream> //  TODO : remove 
+#include <unistd.h>
+
 namespace pr {
 
 #define STACKSIZE 100
 
 template<typename T>
 class Stack {
+	const int N;
 	T tab [STACKSIZE];
 	size_t sz;
-	sem_t *sem_prod;
-	sem_t *sem_cons;
+	sem_t mutex;
+	sem_t sem_prod;
+	sem_t sem_cons;
 public :
-	Stack () : sz(0) {
+	Stack (int N) : N(N), sz(0) {
 		memset(tab,0,sizeof tab);
-		sem_prod = sem_open("sem_prod", O_CREAT|O_EXCL|O_RDWR, 066, 1);
-		sem_cons = sem_open("sem_cons", O_CREAT|O_EXCL|O_RDWR, 066, 1);
+		sem_init(&mutex, 1, 1);
+		sem_init(&sem_cons, 1, 0);
+		sem_init(&sem_prod, 1, N);
 	}
 
 	~Stack(){
-		sem_prod->unlink();
-		sem_cons->unlink();
+		sem_destroy(&mutex);
+		sem_destroy(&sem_cons);
+		sem_destroy(&sem_prod);
 	}
 
 	T pop () {
+		std::cout << std::endl << "[PID:" << getpid() << "] Stack::pop()" << std::endl;
+
 		// bloquer si vide
-		if(sz == 0) {
-			sem_cons->wait();
-		}
+		sem_wait(&sem_cons);
+		sem_wait(&mutex);
 		T toret = tab[--sz];
 		// débloquer producteur
-		sem_prod->post();
+		sem_post(&mutex);
+		sem_post(&sem_prod);
 		return toret;
 	}
 
 	void push(T elt) {
-		//bloquer si plein
-		if(sz == STACKSIZE){
-			sem_prod->wait();
-		}
+		std::cout << std::endl << "[PID:" << getpid() << "] Stack::push()" << std::endl;
 
+		//bloquer si plein
+		sem_wait(&sem_prod);
+		std::cout << "push1" << std::endl;
+		sem_wait(&mutex);
+		std::cout << "push2" << std::endl;
 		tab[sz++] = elt;
+		std::cout << "push3" << std::endl;
 		// débloquer consommateur
-		sem_cons->post();
+		sem_post(&mutex);
+		std::cout << "push4" << std::endl;
+		sem_post(&sem_cons);
+		std::cout << "push5" << std::endl;
 	}
+
+	// faire une méthode stop() pour gérer l'arrêt du programme avec SIGINT.
+	// débloque les sémaphores bloquants
 };
 
 }
--- TME7/src/prod_cons.cpp
+++ TME7/src/prod_cons.cpp
@@ -9,56 +9,92 @@
 using namespace std;
 using namespace pr;
 
+// VARIABLE GLOBALE
+// état du programme (interruption Ctrl+C)
+bool interrupt = false;
+
 void producteur (Stack<char> * stack) {
 	char c ;
-	while (cin.get(c)) {
-		stack->push(c);
+	while(!interrupt && cin.get(c)){
+
+		while (cin.get(c)){
+			cout << "producteur interrupt : " << interrupt << endl;
+			if(interrupt){
+				std::cout<<"j'entre"<<std::endl;
+				return;
+			}
+			stack->push(c);
+		}
 	}
 }
 
 void consomateur (Stack<char> * stack) {
-	while (true) {
-		char c = stack->pop();
-		cout << c << flush ;
-	}
+	
+		while (!interrupt) {
+			cout << "consomateur interrupt : " << interrupt << endl;
+			if(interrupt){
+				std::cout<<"j'entre 2"<<std::endl;
+				return;
+			}
+			char c = stack->pop();
+			cout << c << flush ;
+		}
+	
 }
 
-int main () {
-	Stack<char> * s = new Stack<char>();
+int main () { while(!interrupt)
+	{const int NB_PROD = 10;
 
-/*
-	const int N = 5; //nb producteurs
-	const int M = 10; //nb consommateurs
-
-	// Mémoire partagée
-	int fd = shm_open("/shm_prod_cons", O_RDWR, 0600);
-	if(fd == -1) {
-		perror("shared memory error");
+	int shared_mem = shm_open("/shared_mem", O_CREAT/*|O_EXCL*/|O_RDWR, 0666);
+	if(shared_mem == -1){
+		perror("shm_open error");
 		exit(1);
 	}
 
-	if(ftruncate(fd, /*size ?*//*) == -1){
+	if(ftruncate(shared_mem, sizeof(Stack<char>)) == -1){
 		perror("ftruncate error");
 		exit(1);
 	}
-*/
+
+	void *mmap_ptr = mmap(NULL, sizeof(Stack<char>), PROT_READ|PROT_WRITE, MAP_SHARED, shared_mem, 0);
+	if(mmap_ptr == MAP_FAILED) {
+		perror("mmap error");
+		exit(1);
+	}
+
+	Stack<char> * s = new (mmap_ptr) Stack<char>(NB_PROD);
+
+	// définition signal
+	signal(SIGINT, [](int sig){
+		interrupt = true;
+		cout << "[PID:" << getpid() << "] interrupt" << interrupt << "->";
+		
+		cout << interrupt << endl;
+	});
 
 	pid_t pp = fork();
 	if (pp==0) {
 		producteur(s);
+		cout << "cc1" << endl;
 		return 0;
 	}
-
+    
 	pid_t pc = fork();
+	
 	if (pc==0) {
+		cout << "fils 2" << endl;
 		consomateur(s);
+		cout << "cc2" << endl;
 		return 0;
 	}
 
+	cout << "cc3" << endl;
 	wait(0);
 	wait(0);
 
-	delete s;
+	s->~Stack();
+	shm_unlink("/shared_mem");
+	}
 	return 0;
 }
 
